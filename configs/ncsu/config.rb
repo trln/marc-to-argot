5c6ddff9ba04424a391418fd7acd576c52e48034
Luke Aeschleman <lukeaeschleman@gmail.com>
2017-02-20 12:38:12 -0500

    adding initial ncsu logic

--- configs/ncsu/config.rb
+++ configs/ncsu/config.rb
@@ -39,16 +39,19 @@ settings do
   provide "argot_writer.pretty_print", false
 
   # Comment out/remove if using marc binary
-  # provide "marc_source.type", "xml"
+  provide "marc_source.type", "xml"
 
   # Prevent argot.rb from processing these fields (you will need to provide your own logic)
-  provide "override", %w(institution)
+  provide "override", %w(id institution items)
 
 end
 
 ################################################
 # Primary ID
 ######
+to_field "id", extract_marc(settings["specs"][:id], :first => true) do |rec, acc|
+  acc.collect! {|s| "NCSU#{s}"}
+end
 
 
 ################################################
@@ -67,3 +70,61 @@ to_field "institution", literal("ncsu")
 ################################################
 # Items
 ######
+item_map = {
+  :i => {
+    :key => "barcode"
+  },
+  :c => {
+    :key => "copy_number",
+  },
+  :m => {
+    :key => "location",
+  },
+  :o => {
+    :key => "note",
+  },
+  :a => {
+    :key => "call_number",
+  },
+  :k => {
+    :key => "status",
+  },
+  :t => {
+    :key => "type",
+  },
+  :v => {
+    :key => "volume",
+  }
+}
+
+to_field "items" do |rec, acc|
+
+  Traject::MarcExtractor.cached("999", :alternate_script => false).each_matching_line(rec) do |field, spec, extractor|
+    item = {}
+
+    field.subfields.each do |subfield|
+      code = subfield.code.to_sym
+      if item_map.key?(code)
+        if !item.key?(code)
+            item[item_map[code][:key]] = []
+        end
+        
+        item[item_map[code][:key]] << subfield.value
+        if code == :i
+          if !item["ils_number"].is_a?(Array)
+            item["ils_number"] = []
+          end
+          item["ils_number"] << subfield.value
+        end
+
+        if item_map[code][:translation_map]
+          translation_map = Traject::TranslationMap.new(item_map[code][:translation_map])
+          translation_map.translate_array!(item[item_map[code][:key]])
+        end
+      end
+    end
+
+    acc << item.each_key {|x| item[x] = item[x].join(';')  } if item
+
+  end
+end

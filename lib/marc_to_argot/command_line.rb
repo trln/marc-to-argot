b53568dd45923129be223078acdf2d34d78ab438
Luke Aeschleman <lukeaeschleman@gmail.com>
2017-03-14 12:10:15 -0400

    stable?

--- lib/marc_to_argot/command_line.rb
+++ lib/marc_to_argot/command_line.rb
@@ -38,67 +38,67 @@ module MarcToArgot
     def create(collection, input, output)
 
       data_dir = File.expand_path("../data",File.dirname(__FILE__))
-      spec = MarcToArgot::SpecGenerator.new options.spec_option
-      exit
+      spec = MarcToArgot::SpecGenerator.new options.spec_option.empty? ? collection : options.spec_option
+      marc_specs = spec.generate_spec
+
+      if collection.empty?
+        coll_opt = spec.instance_variable_get(:@collections).join("\n")
+        abort("Plesae specify a collection \n Available options: \n #{coll_opt}")
+      end
       
       if !File.exist?(input)
-
-        warn "no input file"
-
-      else
-
-        flatten_attributes = YAML.load_file("#{data_dir}/flatten_attributes.yml")
-
-        spec = MarcToArgot::SpecGenerator.new options.spec_option
-
-
-        spec_file_path = options.spec_file.empty? ? "#{data_dir}/#{collection}/marc_specs.yml" : options.spec_file
-        spec_yaml = File.exist?(spec_file_path) ? YAML.load_file(spec_file_path) : YAML.load_file("#{data_dir}/argot/marc_specs.yml")
-        specs = MarcToArgot::SpecGenerator::transform_spec(spec_yaml)
-        override = File.exist?("#{data_dir}/#{collection}/overrides.yml") ? YAML.load_file("#{data_dir}/#{collection}/overrides.yml") : []
-
-        settings = {
-          "argot_writer.flatten_attributes" => flatten_attributes,
-          "argot_writer.pretty_print" => options.pretty,
-          "writer_class_name" => "Traject::ArgotWriter",
-          "specs" => specs,
-          "processing_thread_pool" => options.thread_pool,
-          "output_file" => output,
-          "marc_source.type" => options.type,
-          "marc_source.encoding" => options.encoding,
-          "override" => override
-        }
-
-        conf_files = ["#{data_dir}/extensions.rb","#{data_dir}/#{collection}/traject_config.rb","#{data_dir}/argot/traject_config.rb"]
-
-        traject_indexer = Traject::Indexer.new settings
-        conf_files.each do |conf_path|
-          begin
-            traject_indexer.load_config_file(conf_path)
-          rescue Errno::ENOENT, Errno::EACCES => e
-            puts "Could not read configuration file '#{conf_path}', exiting..."
-            exit 2
-          rescue Traject::Indexer::ConfigLoadError => e
-            puts "\n"
-            puts e.message
-            puts e.config_file_backtrace
-            puts "\n"
-            puts "Exiting..."
-            exit 3
-          end
-        end
-
-        traject_indexer.logger.info("traject (#{Traject::VERSION}) executing with: ")
-
-        io = File.open(input, 'r')
-        filename = input
-        traject_indexer.settings['command_line.filename'] = filename if filename
-
-        result = traject_indexer.process(io)
-        
-        return result
-
+        abort('Could not find the input file at #{input}')
       end
+
+      if output.empty?
+        abort('Please provide an output file')
+      end
+
+      flatten_attributes = YAML.load_file("#{data_dir}/flatten_attributes.yml")
+
+      override = File.exist?("#{data_dir}/#{collection}/overrides.yml") ? YAML.load_file("#{data_dir}/#{collection}/overrides.yml") : []
+
+      settings = {
+        "argot_writer.flatten_attributes" => flatten_attributes,
+        "argot_writer.pretty_print" => options.pretty,
+        "writer_class_name" => "Traject::ArgotWriter",
+        "specs" => marc_specs,
+        "processing_thread_pool" => options.thread_pool,
+        "output_file" => output,
+        "marc_source.type" => options.type,
+        "marc_source.encoding" => options.encoding,
+        "override" => override
+      }
+
+      conf_files = ["#{data_dir}/extensions.rb","#{data_dir}/#{collection}/traject_config.rb","#{data_dir}/argot/traject_config.rb"]
+
+      traject_indexer = Traject::Indexer.new settings
+      conf_files.each do |conf_path|
+        begin
+          traject_indexer.load_config_file(conf_path)
+        rescue Errno::ENOENT, Errno::EACCES => e
+          puts "Could not read configuration file '#{conf_path}', exiting..."
+          exit 2
+        rescue Traject::Indexer::ConfigLoadError => e
+          puts "\n"
+          puts e.message
+          puts e.config_file_backtrace
+          puts "\n"
+          puts "Exiting..."
+          exit 3
+        end
+      end
+
+      traject_indexer.logger.info("traject (#{Traject::VERSION}) executing with: ")
+
+      io = File.open(input, 'r')
+      filename = input
+      traject_indexer.settings['command_line.filename'] = filename if filename
+
+      result = traject_indexer.process(io)
+      
+      return result
+
     rescue Exception => e
       # Try to log unexpected exceptions if possible
       traject_indexer && traject_indexer.logger &&  traject_indexer.logger.fatal("Traject::CommandLine: Unexpected exception, terminating execution: #{e.inspect}") rescue nil
--- lib/marc_to_argot/spec_generator.rb
+++ lib/marc_to_argot/spec_generator.rb
@@ -1,26 +1,45 @@
-# Encoding: UTF-8
-
-require 'yaml'
 require 'marc_to_argot'
 
 module MarcToArgot
    
     class SpecGenerator
 
-        @specs = {}
-        @data_dir = File.expand_path("../data",File.dirname(__FILE__))
+        @data_dir = ''
+        @collections = []
+        @spec_file_path = ''
+        @input = ''
             
-        def initialize(spec_file = nil)
-            puts Dir.entries("#{data_dir}")
+        def initialize(input)
+            @input = input
+            @data_dir = File.expand_path("../data",File.dirname(__FILE__))
+            @spec_file_path = File.join(@data_dir,'argot/marc_specs.yml')
+            get_collections
         end
 
-        def self.generate_spec_file(spec_file)
-            spec_config = YAML.load_file(spec_file)
-            config = transform_spec(spec_config)
-            config if !config.empty?
+        def get_collections
+            @collections = Dir.entries("#{@data_dir}").select {|entry| File.directory? File.join(@data_dir,entry) and !(entry =='.' || entry == '..')}
         end
 
-        def self.transform_spec(set)
+        def get_spec_file_path()
+            default = @spec_file_path
+            if @collections.include?(@input)
+                @spec_file_path = File.join(@data_dir,@input,'marc_specs.yml')
+            else
+                if File.exist?(@input)
+                    @spec_file_path = @input
+                end
+            end
+            if default == @spec_file_path
+                warn("Spec file could not be found, using default Argot specs")
+            end
+        end
+
+        def generate_spec()
+            get_spec_file_path
+            return transform_spec(YAML.load_file(@spec_file_path))
+        end
+
+        def transform_spec(set)
             config = {}
             set.each do |k,v|
                 if v.is_a?(Hash)

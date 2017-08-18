cad81241f10a6aa5a7f7c2373ab7f10ac6d0dda5
Luke Aeschleman <lukeaeschleman@gmail.com>
2017-08-18 11:16:36 -0400

    update unc config to generate call number fields

--- lib/data/unc/traject_config.rb
+++ lib/data/unc/traject_config.rb
@@ -41,17 +41,22 @@ end
 # https://github.com/trln/extract_marcxml_for_argot_unc/blob/master/attached_record_data_mapping.csv
 ######
 item_map = {
-  i: { key: 'id'},
-  l: { key: 'library'},
+  i: { key: 'id' },
+  l: { key: 'library' },
   p: { key: 'call_number_scheme' },
   q: { key: 'call_number' },
-  s: { key: 'status' },
+  s: { key: 'status' }
 }
 
-to_field 'items' do |rec, acc|
+to_field 'items' do |rec, acc, ctx|
 
-  Traject::MarcExtractor.cached("999", :alternate_script => false).each_matching_line(rec) do |field, spec, extractor|
-    if field.indicator2 == "1"
+  lcc_top = Set.new
+  formats = marc_formats.call(rec, [])
+  lcc_scheme_codes = %w[090 050]
+  items = []
+
+  Traject::MarcExtractor.cached('999', alternate_script: false).each_matching_line(rec) do |field, spec, extractor|
+    if field.indicator2 == '1'
       item = {}
 
       field.subfields.each do |subfield|
@@ -60,13 +65,16 @@ to_field 'items' do |rec, acc|
         item[mapped] = subfield.value unless mapped.nil?
       end
 
-      if item.fetch('call_number_scheme', '') == '0'
-        item['lcc_top'] = item['call_number'][0,1]
+
+      if lcc_scheme_codes.include?(item.fetch('call_number_scheme', ''))
+        item['call_number_scheme'] = 'LC'
+        lcc_top.add(item['call_number'].gsub!(/\|\w/, '')[0, 1])
       end
+      items << item
+      acc << item.to_json if item
 
     end
-
-    acc << item.to_json if item
-
+    ctx.output_hash['lcc_top'] = lcc_top.to_a
+    map_call_numbers(ctx, items)
   end
 end

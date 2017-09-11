37ea4d1306130585e8f449f3f8ce13e05c88496c
Luke Aeschleman <lukeaeschleman@gmail.com>
2017-09-11 09:35:23 -0400

    adding item status & availability

--- lib/data/unc/traject_config.rb
+++ lib/data/unc/traject_config.rb
@@ -31,13 +31,22 @@ to_field "institution", literal("unc")
 # Items
 # https://github.com/trln/extract_marcxml_for_argot_unc/blob/master/attached_record_data_mapping.csv
 ######
+def status_map
+  @status_map ||=Traject::TranslationMap.new('unc/status_map')
+end
+
+def is_available?(items)
+  items.any? { |i| i['status'].downcase.start_with?('available') rescue false }
+end
+
 item_map = {
   i: { key: 'id' },
   l: { key: 'library' },
   p: { key: 'call_number_scheme' },
   q: { key: 'call_number' },
-  s: { key: 'status' }
+  s: { key: 'status', translation_map: status_map }
 }
+
 
 to_field 'items' do |rec, acc, ctx|
 
@@ -52,8 +61,10 @@ to_field 'items' do |rec, acc, ctx|
 
       field.subfields.each do |subfield|
         code = subfield.code.to_sym
-        mapped = item_map.fetch(code, key: nil)[:key]
-        item[mapped] = subfield.value unless mapped.nil?
+        map_hash = item_map.fetch(code, key: nil)
+        unless map_hash.nil?
+          item[map_hash[:key]] = map_hash[:translation_map] ? map_hash[:translation_map][subfield.value] : subfield.value
+        end
       end
 
 
@@ -66,6 +77,7 @@ to_field 'items' do |rec, acc, ctx|
 
     end
     ctx.output_hash['lcc_top'] = lcc_top.to_a
+    ctx.output_hash['available'] = 'Available' if is_available?(items)
     map_call_numbers(ctx, items)
   end
 end

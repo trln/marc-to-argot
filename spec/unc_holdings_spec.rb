74815e02205db1f4963bebbc69961b420a256b79
Kristina Spurgin <kristina@infomuse.net>
2017-11-06 12:38:12 -0500

    add location test

---
+++ spec/unc_holdings_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'marc_to_argot'
+
+describe MarcToArgot do
+b1246383argot = JSON.parse( TrajectRunTest.run_traject('unc', 'b1246383') )
+
+    it 'sets holdings locations' do
+    expect(b1246383argot['holdings']).to(
+        include("\"loc_b\":\"trln\",\"loc_n\":\"trln\"")
+    )
+    end
+
+end

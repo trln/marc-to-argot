8ecf15ebb4ff906b6ea1fcdd8e18f5035669736f
Kristina Spurgin <kristina@infomuse.net>
2017-11-06 12:45:30 -0500

    tweak test

--- spec/unc_holdings_spec.rb
+++ spec/unc_holdings_spec.rb
@@ -5,7 +5,7 @@ describe MarcToArgot do
 b1246383argot = JSON.parse( TrajectRunTest.run_traject('unc', 'b1246383') )
 
     it 'sets holdings locations' do
-    expect(b1246383argot['holdings']).to(
+    expect(b1246383argot['holdings'][0]).to(
         include("\"loc_b\":\"trln\",\"loc_n\":\"trln\"")
     )
     end

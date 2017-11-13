2bc8b87e8e5c93c0cedb65e83d868e57755564f3
U-AD\kspurgin <kspurgin@AALTS60.lib.unc.edu>
2017-11-12 20:34:22 -0500

    test nucmc

--- spec/mta_ids_spec.rb
+++ spec/mta_ids_spec.rb
@@ -11,4 +11,11 @@ describe MarcToArgot do
       eq({'id' => 'sn 78003579', 'qual' => '', 'type' => 'LCCN'})
     )
   end
+
+  it '(MTA) sets NUCMC' do
+    result = ids01['misc_id'][2]
+    expect(result).to(
+      eq({'id' => 'ms 69001649', 'qual' => '', 'type' => 'NUCMC'})
+    )
+  end
 end

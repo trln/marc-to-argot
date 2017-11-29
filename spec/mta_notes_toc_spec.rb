8068d1b2ccd2c1ee12f1258b7f2df9719e3176e5
Kristina Spurgin <kristina@infomuse.net>
2017-11-29 13:41:17 -0500

    note_toc works

--- spec/mta_notes_toc_spec.rb
+++ spec/mta_notes_toc_spec.rb
@@ -5,6 +5,8 @@ describe MarcToArgot do
   toc02 = JSON.parse( Util::TrajectRunTest.run_traject('unc', 'toc02') )
   toc03 = JSON.parse( Util::TrajectRunTest.run_traject('unc', 'toc03') )  
 
+  # only subfields agrt will be kept/displayed.
+  # subfield u allows a URL to be put in the field. Do we want that? 
   it '(MTA) sets single basic TOC note' do
     result = toc01['note_toc']
     expect(result).to(
@@ -12,6 +14,7 @@ describe MarcToArgot do
     )
   end
 
+  # Provides 'Incomplete contents:' based on 1st indicator = 1
   it '(MTA) sets single enhanced incomplete TOC note' do
     result = toc02['note_toc']
     expect(result).to(
@@ -19,10 +22,13 @@ describe MarcToArgot do
     )
   end
 
+  # Provides 'Partial contents:' based on 1st indicator = 2
+  # Strips trailing spaces from subfields
+  # Keeps multiple 505s in order
   it '(MTA) sets multiple enhanced partial TOC notes' do
     result = toc03['note_toc']
     expect(result).to(
-      eq(['Partial contents: Non-indexed contextual info: Chapter 1 title / Chapter 1 author -- Chapter 2 title / Chapter 2 author --', 'Round 2: Chapter 3 title / Chapter 3 author -- Chapter 4 title / Chapter 4 author.'])
+      eq(['Partial contents: Non-indexed contextual info: Chapter 1 title / Chapter 1 author -- Chapter 2 title / Chapter 2 author --', 'ARound 2: Chapter 3 title / Chapter 3 author -- Chapter 4 title / Chapter 4 author.'])
     )
   end
 end

5213a03f63e2052058db1013b8c3cc622b9b5371
Kristina Spurgin <kspurgin@email.unc.edu>
2017-11-30 16:41:13 -0500

    unc override access_type

--- lib/marc_to_argot/macros/unc_macros.rb
+++ lib/marc_to_argot/macros/unc_macros.rb
@@ -3,6 +3,35 @@ module MarcToArgot
     # Macros and useful functions for UNC records
     module UNC
       include MarcToArgot::Macros::Shared
+      # values to look for in the 856 that indicate
+      # a record has online access.
+      ELOC_IND2 = Set.new(%w[0 1])
+
+      # tests whether the record has an 856[ind2] that matches
+      # any of the values in ELOC_IND2
+      # @param rec [MARC::Record] the record to be checked.
+      # @param _ctx [Object] extra context or data to be used in the test
+      #   (for overrides)
+      def online_access?(rec, _ctx = {})
+        l = rec.fields('856')
+        return false if l.nil?
+        !l.find { |f| ELOC_IND2.include?(f.indicator2) }.nil?
+      end
+
+      # tests whether the record has any physical items
+      # this implementation asks whether there are any 999 fields.
+      # @param rec [MARC::Record] the record to be checked.
+      # @param _ctx [Object] extra context or data to be used in the test
+      #   (for overrides)
+      def physical_access?(rec, _ctx = {})
+        checkfields = []
+        rec.each_by_tag('999') { |f| checkfields << f if f.indicator1 == '9' }
+        if checkfields.size > 0
+          return true
+        else
+          return false
+        end
+      end
     end
   end
 end

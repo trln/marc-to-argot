3007eeaf2bdd175afa0a324e14024d840d00a89e
Kristina Spurgin <kspurgin@email.unc.edu>
2017-12-01 18:16:59 -0500

    progress on url

--- lib/marc_to_argot/macros/unc_macros.rb
+++ lib/marc_to_argot/macros/unc_macros.rb
@@ -2,6 +2,8 @@ module MarcToArgot
   module Macros
     # Macros and useful functions for UNC records
     module UNC
+      MarcExtractor = Traject::MarcExtractor
+      include Traject::Macros::Marc21Semantics
       include MarcToArgot::Macros::Shared
       
       # tests whether the record has any physical items
@@ -23,6 +25,59 @@ module MarcToArgot
           return false
         end
       end
+
+      
+
+      def url
+        url_extor = MarcExtractor.cached('856uy3')
+        lambda do |rec, acc, _|
+          urls = []
+          url_extor.each_matching_line(rec) do |field, spec, extractor|
+            url = {}
+            text = []
+            text3 = []
+            rel = ''
+            href = []
+
+            field.subfields.each do |sf|
+              val = sf.value.strip
+              case sf.code
+              when 'u'
+                href << val
+              when 'y'
+                text << val
+              when '3'
+                text3 << val
+                rel = 'thumbnail' if val.downcase.include?('thumbnail')
+                rel = 'findingaid' if val.downcase.include?('finding aid')
+              end
+            end
+
+            url['href'] = href.first unless href.empty?
+
+            case field.indicator2.to_i
+            when 0
+              rel = 'fulltext'
+            when 1
+              rel = 'fulltext'
+            when 2
+              rel = 'related' if rel.empty?
+            else
+              rel = 'other'
+            end
+
+            # don't need this delimiter unless both text and text3 are populated
+            text << 'Available via the UNC-Chapel Hill Libraries' if text.empty? && rel == 'fulltext'
+            text3 << '--' unless text3.empty? || text.empty?
+            finaltext = text3 + text
+            url['text'] = finaltext.join(' ')
+            url['rel'] = rel
+            urls << url unless href.empty?
+          end
+          urls.each { |u| acc << u } unless urls.empty?
+        end
+      end
+      
     end
   end
 end

efecd11899fa70782331f31571af91ee4ff9854f
Cory Lown <cory.lown@duke.edu>
2018-05-08 08:46:55 -0400

    Add/refactor misc_id

--- spec/mta_ids_spec.rb
+++ spec/mta_ids_spec.rb
@@ -3,29 +3,29 @@ require 'spec_helper'
 describe MarcToArgot do
   include Util::TrajectRunTest
   let(:ids01) { run_traject_json('unc', 'ids01') }
-  
+
   # -=-=-=-=-=-=-=-
   # 010 tests
-  # -=-=-=-=-=-=-=-  
+  # -=-=-=-=-=-=-=-
   it '(MTA) sets LCCN' do
     result = ids01['misc_id'][0]
     expect(result).to(
-      eq({'value' => 'sn 78003579', 'qual' => '', 'type' => 'LCCN'})
+      eq({'value' => 'sn 78003579', 'type' => 'LCCN'})
     )
   end
 
   it '(MTA) sets NUCMC' do
     result = ids01['misc_id'][2]
     expect(result).to(
-      eq({'value' => 'ms 69001649 ', 'qual' => '', 'type' => 'NUCMC'})
+      eq({'value' => 'ms 69001649', 'type' => 'NUCMC'})
     )
   end
 
   # -=-=-=-=-=-=-=-
   # 015 tests
-  # -=-=-=-=-=-=-=-  
+  # -=-=-=-=-=-=-=-
   it '(MTA) sets national bib number (1 a, 1 q, 2 lookup)' do
-    result = ids01['misc_id'][3]
+    result = ids01['misc_id'][4]
     expect(result).to(
       eq({'value' => '123', 'qual' => 'v. 1', 'type' => "Bibliografía d'Andorra"})
     )
@@ -33,24 +33,24 @@ describe MarcToArgot do
 
   it '(MTA) sets national bib number (1 a containing qual, 0 q, no 2 lookup)' do
     result = [
-      ids01['misc_id'][4],
       ids01['misc_id'][5],
-      ids01['misc_id'][6]
+      ids01['misc_id'][6],
+      ids01['misc_id'][7]
     ]
     expect(result).to(
       eq([
            {'value' => '123', 'qual' => 'v. 1', 'type' => "National Bibliography Number"},
            {'value' => '789', 'qual' => 'v. 2', 'type' => "National Bibliography Number"},
-           {'value' => '1010', 'qual' => '', 'type' => "National Bibliography Number"}
+           {'value' => '1010', 'type' => "National Bibliography Number"}
          ])
     )
   end
 
   it '(MTA) sets national bib number with no qual when entire $a value in parens' do
-    result = ids01['misc_id'][7]
+    result = ids01['misc_id'][8]
     expect(result).to(
       eq(
-        {'value' => '(USSR 68-VKP)', 'qual' => '', 'type' => "National Bibliography Number"}
+        {'value' => '(USSR 68-VKP)', 'type' => "National Bibliography Number"}
       )
     )
   end

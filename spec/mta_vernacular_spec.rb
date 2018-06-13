1804ce5e6c7fc9761e8694b32c8140d6c5365527
Kristina Spurgin <kspurgin@email.unc.edu>
2018-06-13 17:47:53 -0400

    add vernacular title tests

--- spec/mta_vernacular_spec.rb
+++ spec/mta_vernacular_spec.rb
@@ -4,13 +4,27 @@ require 'spec_helper'
 describe MarcToArgot do
   include Util::TrajectRunTest
   let(:title1) { run_traject_json('unc', 'vern_title1', 'mrc') }
-
-  it '(MTA) sets included_work' do
-    result = title1['title']['main']
+  let(:title2) { run_traject_json('unc', 'vern_title2', 'mrc') }
+  
+  xit '(MTA) sets vernacular title_main from 880' do
+    result = title1['title_main']
     expect(result).to eq(
-                        [{'value'=>'',
-                          'author'=>'Saint-SaÃ«ns, Camille, 1835-1921.',
-                          'title'=>['Quartets,', 'violins (2), viola, cello,', 'no. 2, op. 153,', 'G major']}                        ]
+                        [
+                          {'value'=>'Urbilder ; Blossoming ; Kalligraphie ; O Mensch, bewein\' dein\' Sünde gross (Arrangement) : for string quartet'},
+                          {'value'=>'原像 ; 開花 ; 書 （カリグラフィー） ほか : 弦楽四重奏のための',
+                           'lang'=>'cjk'}
+                        ]
                       )
   end
+
+  xit '(MTA) sets vernacular title_main from 245' do
+    result = title2['title_main']
+    expect(result).to eq(
+                        [
+                          {'value'=>'近代日本文学研究の問題点',
+                           'lang'=>'cjk'}
+                        ]
+                      )
+  end
+  
 end

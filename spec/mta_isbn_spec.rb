88a76097ec5b9be36cc7e9c7f3dbce2ac6c555b0
Kristina Spurgin <kspurgin@email.unc.edu>
2018-06-14 17:49:44 -0400

    add tests for vernacular isbn data

---
+++ spec/mta_isbn_spec.rb
@@ -0,0 +1,28 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe MarcToArgot do
+  include Util::TrajectRunTest
+  let(:visbn1) { run_traject_json('unc', 'vern_isbn1', 'mrc') }
+
+  
+  xit '(MTA) sets vernacular isbn qualifiers' do
+    result = visbn1['isbn']
+    expect(result).to eq(
+                        [
+                          {
+                            'number'=>'9575433742',
+                            'qualifying_info'=>'ping zhuang'
+                          },
+                          {
+                            'number'=>'9789575433741',
+                            'qualifying_info'=>'ping zhuang'
+                          },
+                          {
+                            'number'=>'9575433742',
+                            'qualifying_info'=>'平裝'
+                          } 
+                        ])
+  end
+
+end

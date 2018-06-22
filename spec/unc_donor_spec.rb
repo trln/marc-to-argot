57905aafabc46cb7d02bdd1c1f0129fb94462d09
Kristina Spurgin <kspurgin@email.unc.edu>
2018-06-22 12:49:13 -0400

    extract unc donor

--- spec/unc_donor_spec.rb
+++ spec/unc_donor_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe MarcToArgot do
   include Util::TrajectRunTest
   let(:corpdonor1) { run_traject_json('unc', '791-1') }
+  let(:donor2) { run_traject_json('unc', 'donor_personal1', 'mrc') }
   
-  it '(UNC) sets a local note from 791 field with indicators 2b' do
+  it '(UNC) sets donor from 791 field with indicators 2b' do
     expect(corpdonor1['donor']).to(
       eq([
            {
@@ -12,6 +13,16 @@ describe MarcToArgot do
            },
            {
              'value' => 'Donated by Scaglione, Aldo D.'
+           }
+         ])
+    )
+  end
+
+  it '(UNC) sets donor without including relators' do
+    expect(donor2['donor']).to(
+      eq([
+           {
+             'value' => 'Donated by Engstrom, Alfred G., 1907-1990'
            }
          ])
     )

68149a99bc123f2bfeb787a5d1a31afb518f748f
Kristina Spurgin <kspurgin@email.unc.edu>
2018-08-08 18:33:38 -0400

    in case of power outage...

--- lib/marc_to_argot/macros/unc/holdings.rb
+++ lib/marc_to_argot/macros/unc/holdings.rb
@@ -25,76 +25,62 @@ module MarcToArgot
 
         def holdings(rec, cxt)
           unless cxt.clipboard[:shared_record_set] == 'dws'
-            holdings = create_holdings_for_processing(rec)
-            holdings.each { |hrec| process_holdings_data(hrec) }
-            
-            cxt.output_hash['holdings'] = 
+            cxt.output_hash['holdings'] = create_holdings_for_processing(rec)
           end
         end
 
-        # creates HoldingsRecords with relevant fields for display and processing
-        # after doing this, we can process the fields without having to check III
-        #  field type constantly
         def create_holdings_for_processing(rec)
           holdings = {}
-          occ = 0
           Traject::MarcExtractor.cached("999|92|").each_matching_line(rec) do |field, spec, extractor|
             id = field['a']
             loc = field['b']
             ct = field['c']
-            occ = occ += 1
-
-            holdings[id] = HoldingsRecord.new(id, loc, ct, occ)
+            holdings[id] = HoldingsRecord.new(id, loc, ct)
           end
 
           Traject::MarcExtractor.cached("999|93|").each_matching_line(rec) do |field, spec, extractor|
             recid = field['0']
-              holdings[recid].fields << new_data_field(field)
-if field['2'] == '852' && field['3'] == 'c'
-            elsif field['3'] == 'h'
-              holdings[recid].fields << new_data_field(field)
+            if field['2'] == '852' && field['3'] == 'c'
+              holdings[recid].fields << field.to_s
             end
           end
+
+          ha = []
+          holdings.each_pair { |k, v| ha << "#{k} -- #{v.fields[0]}" }
+          ha
         end
 
-        def new_data_field(field)
-          datafield = MARC::DataField.new(field['2'],
-                                          field.indicator1,
-                                          field.indicator2)
-          field.subfields.each do |sf|
-            unless %w[0 2 3].include?(sf.code)
-              datafield << [sf.code, sf.value]
-            end
-          end
+        def extract_call_number(field)
+          subfield_z = field.find_all {|subfield| subfield.code == 'z'}
+          call_number = field.find_all { |sf| %w[hijk
+          hijk
         end
 
-        def process_holdings_data(hrec)
-        end
+        #   # get array of holdings fields relevant to this processing/display
+        #   holdings_fields = []
+        #   Traject::MarcExtractor.cached("999|92|:999|93|").each_matching_line(rec) do |field, spec, extractor|
+        #     holdings_fields << field if field.indicator2 == '2' ||
+        #                                 (field.indicator2 == '3' && (field.tag == '852' ||
+        #                                                              field['3'] == 'h')
+        #                                 )
+            
+        #   end
+
+          
+        # end
 
         class HoldingsRecord
-          attr_reader :id
-          attr_reader :loc
-          attr_reader :card_ct
-          attr_accessor :call_numbers
-          attr_accessor :fields
-          attr_reader :occ
+          attr :id
+          attr :loc
+          attr :card_ct
+          attr :fields
 
-          def initialize(id, loc, ct, occ)
+          def initialize(id, loc, ct)
             @id = id
             @loc = loc
             @card_ct = ct
-            @call_numbers = []
             @fields = []
-            @occ = occ
           end
-
-          def self.extract_call_numbers
-            cn_fields = @fields.find_all { |f| 
-            call_number_elements = field.find_all { |sf| %w[h i j k].include?(sf.code) }
-            return call_number_elements.join(' ') if !call_number_elements.empty?
-          end
-
-
         end
 
         class HoldingsField < MARC::DataField

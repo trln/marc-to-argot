4b45970995efd358e2950e6a2cf82d8cc9f0bde7
Kristina Spurgin <kspurgin@email.unc.edu>
2018-08-08 09:47:16 -0400

    archives, manuscript, thesis handling

--- lib/marc_to_argot/macros/unc/resource_type.rb
+++ lib/marc_to_argot/macros/unc/resource_type.rb
@@ -13,21 +13,59 @@ module MarcToArgot
         class UncResourceTypeClassifier < ResourceTypeClassifier
           attr_reader :record
           
-          def initialize(rec)
-            super
-          end
-
           def get_general_formats
             UncResourceTypeClassifier.new(record).formats
           end
           
           def unc_formats
             formats = get_general_formats
+            if unc_archival?
+              formats << 'Archival and manuscript material'
+            end
+            if unc_manuscript?
+              formats = []
+              formats << 'Archival and manuscript material'
+            end
+            if unc_manuscript?
+              formats = []
+              formats << 'Archival and manuscript material'
+            end
             if unc_thesis_dissertation?
               formats.delete('Archival and manuscript material')
+              formats.delete('Book')
               formats << 'Thesis/Dissertation'
+              formats << 'Book' unless has_502?
             end
-            formats
+            formats.uniq
+          end
+
+          def manuscript_lang_rec_type?
+            record.leader.byteslice(6) == 't'
+          end
+
+          def lang_rec_type?
+            record.leader.byteslice(6) == 'a'
+          end
+
+          def unc_archival?
+            archival_control? && archival_bib_level?
+          end
+
+          def archival_control?
+            record.leader.byteslice(8) == 'a'
+          end
+
+          def archival_bib_level?
+            %w[c d].include?(record.leader.byteslice(7))
+          end
+
+          def book_008?
+            return true if ( manuscript_lang_rec_type? ||
+                             ( lang_rec_type? && %w[a c d m].include?(record.leader.byteslice(7)))
+          end
+          
+          def unc_manuscript?
+            return true if manuscript_lang_rec_type? unless has_502?
           end
           
           # Thesis/Dissertation
@@ -37,7 +75,7 @@ module MarcToArgot
           # OR
           # 006/00 = a AND 006/07-10(any) = m
           def unc_thesis_dissertation?
-            rec_type_match = %[a t].include?(record.leader.byteslice(6))
+            rec_type_match = manuscript_lang_rec_type? || lang_rec_type?
             nature_contents_match = record.fields('008').find do |field|
               (field.value.byteslice(24..27) || '').split('').include?('m')
             end
@@ -48,8 +86,13 @@ module MarcToArgot
             end
 
             return true if (rec_type_match && nature_contents_match) ||
-                           marc_006_match_results.include?(true)
-          end  
+                           marc_006_match_results.include?(true) ||
+                           rec_type_match && has_502?
+          end
+
+          def has_502?
+            return true unless record.fields('502').empty?
+          end
 
         end
       end

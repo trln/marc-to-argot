9c70fc4098cf5586085098d496302965e7675367
Cory Lown <cory.lown@duke.edu>
2018-10-10 10:24:20 -0400

    Add vernacular handling for imprint and publisher fields

--- spec/mta_publisher_spec.rb
+++ spec/mta_publisher_spec.rb
@@ -7,13 +7,28 @@ describe MarcToArgot do
     let(:records) { MARC::XMLReader.new(find_marc('unc', 'imprint')).to_a }
     let(:publisher_expectation) {
       [
-        ['Writers and Readers Pub. Cooperative Society', 'Distributed in the U.S.A. by W.W. Norton'],
-        ['Published by the editors in cooperation with the School of Liberal Arts at North Carolina State of the University of North Carolina', 'English Dept., UNCC', 'Advancment Studies, CPCC', 'English Dept., CPCC', 'Dept. of Languages, Literature & Philosophy, Armstrong Atlantic State University'],
-        ['Dodd, Mead', 'Beacon Press', 'Chilton Book Co.', 'Applause Theater Book Publishers'],
-        ['Channel Four (Great Britain)', 'Films Media Group'],
-        ['Douglass College, Rutgers University', 'Transaction Publishers'],
-        ['"N. Niva, "', 'O.D. Strokʺ', 'publisher not identified', '"Н. Нива, "', 'О.Д. Строкъ'],
-        ['William S. Hein & Co.', 'William S. Hein & Company']
+        [{ 'value' => 'Writers and Readers Pub. Cooperative Society' },
+         { 'value' => 'Distributed in the U.S.A. by W.W. Norton' }],
+        [{ 'value' => 'Published by the editors in cooperation with the School of Liberal Arts '\
+                      'at North Carolina State of the University of North Carolina' },
+         { 'value' => 'English Dept., UNCC' },
+         { 'value' => 'Advancment Studies, CPCC' },
+         { 'value' => 'English Dept., CPCC' },
+         { 'value' => 'Dept. of Languages, Literature & Philosophy, Armstrong Atlantic State University' }],
+        [{ 'value' => 'Dodd, Mead' },
+         { 'value' => 'Beacon Press' }, {'value'=> 'Chilton Book Co.' },
+         { 'value' => 'Applause Theater Book Publishers' }],
+        [{ 'value' => 'Channel Four (Great Britain)' },
+         { 'value' => 'Films Media Group' }],
+        [{ 'value' => 'Douglass College, Rutgers University' },
+         { 'value' => 'Transaction Publishers' }],
+        [{ 'value' => '"N. Niva, "' },
+         { 'value' => 'O.D. Strokʺ' },
+         { 'value' => 'publisher not identified' },
+         { 'value' => '"Н. Нива, "', 'lang'=> 'rus' },
+         { 'value' =>  'О.Д. Строкъ', 'lang'=> 'rus' }],
+        [{ 'value' => 'William S. Hein & Co.' },
+         { 'value' => 'William S. Hein & Company' }]
       ]
     }
 
@@ -28,14 +43,14 @@ describe MarcToArgot do
 
     let(:imprint_v1) { run_traject_json('unc', 'imprint_v1', 'mrc') }
 
-    xit '(MTA) sets publisher from 260 and linked 880' do
+    it '(MTA) sets publisher from 260 and linked 880' do
       result = imprint_v1['publisher']
       expect(result).to eq(
                           [
-                            {'value': 'In-t vseobshcheĭ istorii RAN'},
-                            {'value': 'Nauka'},
-                            {'value': 'Ин-т всеобщей истории РАН', 'lang': 'rus'},
-                            {'value': 'Наука', 'lang': 'rus'} 
+                            { 'value' => 'In-t vseobshcheĭ istorii RAN' },
+                            { 'value' => 'Nauka' },
+                            { 'value' => 'Ин-т всеобщей истории РАН', 'lang'=> 'rus' },
+                            { 'value' => 'Наука', 'lang'=> 'rus' }
                           ]
                         )
     end

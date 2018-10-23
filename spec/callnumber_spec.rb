035bd2d6b6cd5948677aa75294d37591462a07ad
Cory Lown <cory.lown@duke.edu>
2018-10-23 15:44:53 -0400

    Change lcc_callnum_classification delimiter to a pipe.

--- spec/callnumber_spec.rb
+++ spec/callnumber_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'marc_to_argot'
 
 describe MarcToArgot::CallNumbers do
+  include Util::TrajectRunTest
   # helps namespace calls to LCC classes
   class Testy
     include MarcToArgot::CallNumbers
@@ -30,4 +31,15 @@ describe MarcToArgot::CallNumbers do
                      'AC9 - AC195 Other languages']
     expect(@test.path('AC 101')).to eq(expected_path)
   end
+
+  let(:item) { run_traject_json('duke', 'items', 'mrc') }
+  it '(MTA) sets lcc_callnum_classification facet values' do
+    result = item['lcc_callnum_classification']
+    expect(result).to eq(
+        ["D - History (General) and History of Europe",
+        "D - History (General) and History of Europe|DF10 - DF951 History of Greece",
+        "D - History (General) and History of Europe|DF10 - DF951 History of Greece|DF10 - DF289 Ancient Greece"]
+      )
+  end
 end
+

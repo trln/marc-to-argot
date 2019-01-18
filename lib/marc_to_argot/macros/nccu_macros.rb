7014beebb6300a88691f06580f31d38fe2f7ec56
Gena Kazymova <genia@trln.org>
2019-01-18 10:34:27 -0500

    Distinguish urls: Online Access (NCCU Only) vs Open Access

--- lib/marc_to_argot/macros/nccu_macros.rb
+++ lib/marc_to_argot/macros/nccu_macros.rb
@@ -17,6 +17,30 @@ module MarcToArgot
         substring_present_in_subfield?(fld, 'u', 'https://finding-aids.lib.unc.edu/')
       end
 
+      def url
+        lambda do |rec, acc|
+          Traject::MarcExtractor.cached("856uy3").each_matching_line(rec) do |field, spec, extractor|
+            url = {}
+            url[:href] = url_href_value(field)
+
+            next if url[:href].nil? || url[:href].empty?
+
+            url[:type] = url_type_value(field)
+            url[:text] = url_text(field) unless url_text(field).empty?
+            url[:note] = url_note(field) unless url_note(field).empty?
+            url[:restricted] = 'false' unless url_restricted?(url[:href], url[:type])
+
+            acc << url.to_json if !acc.include?(url.to_json)
+
+          end
+        end
+      end
+
+      def url_restricted?(href, type)
+        return false if href.match(/(\.edu)|(\.gov)/) && type == 'fulltext'
+        true
+      end
+
       def rollup_id
         lambda do |rec, acc|
           Traject::MarcExtractor.cached("035a").each_matching_line(rec) do |field, spec, extractor|

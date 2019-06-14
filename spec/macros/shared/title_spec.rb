b1011bf03ef59413552ed71658fc01c6a44fc5f6
Kristina Spurgin <kspurgin@email.unc.edu>
2019-06-14 16:58:02 -0400

    test short_title on vernacular

--- spec/macros/shared/title_spec.rb
+++ spec/macros/shared/title_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 describe MarcToArgot::Macros::Shared::Title do
   include described_class
 
@@ -34,4 +35,33 @@ describe MarcToArgot::Macros::Shared::Title do
       expect(acc).to be_empty
     end
   end
+
+  context 'short title in 245 and linked 880 present' do
+      xit '(MTA) sets short_title from both?' do
+        rec = make_rec
+        rec << MARC::DataField.new('245', '0', '1',
+                                   ['6', '880-01'],
+                                   ['a', 'Ubileĭnoe izd'])
+        rec << MARC::DataField.new('880', '0', '1',
+                                   ['6', '245-01'],
+                                   ['a', 'Юбилейное изд'])
+        result = run_traject_on_record('unc', rec)['short_title']
+        expect(result).to eq(
+                            [ 'Ubileĭnoe izd', 'Юбилейное изд' ]
+                          )
+      end
+  end
+
+  context 'non-Roman short title in 245' do
+    xit '(MTA) sets non-Roman short title' do
+      rec = make_rec
+      rec << MARC::DataField.new('245', '1', '0',
+                                 ['a', 'Юбилейное изд'])
+      result = run_traject_on_record('unc', rec)['short_title']
+      expect(result).to eq(
+                          [ 'Юбилейное изд' ]
+                        )
+    end
+  end
+
 end

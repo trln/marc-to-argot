dfd9ffb47920fb6677d3d7f815c94a1f0581582c
Kristina Spurgin <kspurgin@email.unc.edu>
2019-06-17 17:11:13 -0400

    map LC call numbers from bibs as well as items

--- lib/marc_to_argot/macros/unc/call_number.rb
+++ lib/marc_to_argot/macros/unc/call_number.rb
@@ -15,55 +15,68 @@ module MarcToArgot
         #   `lcc_callnum_classification`
 
         def process_call_numbers(rec, cxt)
+          out = cxt.output_hash
+          if out['items']
+            items = out['items'].map{ |i| JSON.parse(i) }
+            item_call_numbers = extract_call_numbers(items)
+          end
+          
+          bib_call_numbers = extract_bib_LC_call_numbers(rec)
 
+
+          unless bib_call_numbers['LC'].empty?
+            if item_call_numbers.nil?
+              item_call_numbers = { 'LC' => bib_call_numbers['LC'] }
+            elsif item_call_numbers.has_key?('LC')
+              bib_call_numbers['LC'].each { |cn| item_call_numbers['LC'] << cn }
+              item_call_numbers['LC'].uniq!
+            else
+              item_call_numbers['LC'] = bib_call_numbers['LC']
+            end
+          end
+
+          if item_call_numbers
+            cxt.output_hash['call_number_schemes'] = item_call_numbers.keys
+            map_shelfkeys!(out, item_call_numbers)
+            map_multi_callnum_classification(out, item_call_numbers)
+          end
 
         end
         
-        def map_call_numbers!(ctx, items)
-          call_numbers = extract_call_numbers(items)
-          out = ctx.output_hash
-          ctx.output_hash['call_number_schemes'] = call_numbers.keys
-          map_shelfkeys!(out, call_numbers)
-          map_callnum_classification!(out, call_numbers)
-        end
-
-        def map_shelfkeys!(output_hash, call_numbers)
-          %w[LC SUDOC].each do |scheme|
-            numbers = call_numbers.fetch(scheme, [])
-            key = numbers.first
-            next unless key
-            output_hash['shelfkey'] = "#{scheme.downcase}:#{key}"
-            output_hash['reverse_shelfkey'] = "#{scheme.downcase}:#{ShelfKeys.reverse_shelfkey(key)}"
-            break
-          end
-        end
-
         private
 
-        def map_callnum_classification!(out, call_numbers)
+        def extract_bib_LC_call_numbers(rec)
+          lc_call_numbers = []
+          Traject::MarcExtractor.cached('050:090', alternate_script: false).each_matching_line(rec) do |field, spec, extractor|
+            cutter = field['b']
+            cns = field.select{ |sf| sf.code == 'a' }.map{ |sf| sf.value }
+            next if cns.length == 0
+            cns.each do |cn|
+              if cutter
+                lc_call_numbers << "#{cn} #{cutter}"
+              else
+                lc_call_numbers << cn
+              end
+            end
+          end
+          return { 'LC' => lc_call_numbers.map{ |cn| LCC.normalize(cn) }.uniq }
+        end
+
+        def map_multi_callnum_classification(out, call_numbers)
           return unless call_numbers.key?('LC')
-          raise ValueError, "#{call_numbers} has no LC call numbers" unless call_numbers['LC'].first
+          raise ValueError, "#{call_numbers} has no LC call numbers" unless call_numbers['LC']
           res = []
-          LCC.find_path(call_numbers['LC'].first).each_with_object([]) do |part, acc|
-            acc << part
-            res << acc.join('|')
+          call_numbers['LC'].each do |callnum|
+            LCC.find_path(callnum).each_with_object([]) do |part, acc|
+              acc << part
+              res << acc.join('|')
+            end
           end
           out['lcc_callnum_classification'] = res
         end
 
-        def extract_call_numbers(items)
-          items.each_with_object({}) do |i, cns|
-            scheme = i['cn_scheme']
-            next unless %w[LC SUDOC].include?(scheme)
-            numbers = (cns[scheme] ||= [])
-            numbers << if scheme == 'LC'
-                         LCC.normalize(i['call_no'])
-                       else
-                         i['call_no']
-                       end
-          end
-        end
       end
     end
   end
 end
+
--- spec/macros/unc/call_number_spec.rb
+++ spec/macros/unc/call_number_spec.rb
@@ -16,17 +16,198 @@ describe MarcToArgot::Macros::UNC::CallNumber do
     end
   end
 
-  describe 'collection_or_subunit?' do
-    xit '(UNC) returns true if LDR/07 = c or d' do
-      rec = make_rec
-      rec.leader[7] = 'c'
-         rec << MARC::DataField.new('856', '4', '2',
-                                 ['u', 'http://finding-aids.lib.unc.edu/03287/'])
-         expect(collection_or_subunit?(rec)).to eq(true)
 
-      rec2 = make_rec
-      rec2.leader[7] = 'd'
-      expect(collection_or_subunit?(rec2)).to eq(true)
+    context 'different LC call numbers in two item records ' do
+      it '(UNC) sets call_number_schemes to LC' do
+        rec = make_rec
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aML96.4 .B3'],
+                                   ['v', 'Bd.1'])
+
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aHV9468.G75|bR33 2017'],
+                                   ['v', 'Bd.2'])
+
+        result = run_traject_on_record('unc', rec)['call_number_schemes']
+        expect(result).to(
+          eq(['LC'])
+        )
+      end
+      it '(UNC) uses first occurring LC call number to set shelfkey (and reverse) values' do
+        rec = make_rec
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aML96.4|b.B3'],
+                                   ['v', 'Bd.1'])
+
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aHV9468.G75|bR33 2017'],
+                                   ['v', 'Bd.2'])
+
+        result = run_traject_on_record('unc', rec)
+        expect(result['shelfkey']).to(
+          eq('lc:ML.00964.B3')
+        )
+        expect(result['reverse_shelfkey']).to(
+          eq('lc:DE}ZZQTV}OW')
+        )
+      end
+
+      it '(UNC) sets lcc_callnum_classification from both LC numbers' do
+        rec = make_rec
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aML96.4|b.B3'],
+                                   ['v', 'Bd.1'])
+
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aHV9468.G75|bR33 2017'],
+                                   ['v', 'Bd.2'])
+
+        result = run_traject_on_record('unc', rec)['lcc_callnum_classification'].sort
+        expect(result).to(
+          eq([
+               'H - Social sciences',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration|HV8301 - HV9920.7 Penology. Prisons. Corrections',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration|HV8301 - HV9920.7 Penology. Prisons. Corrections|HV9441 - HV9920.7 By region or country',
+               'M - Music',
+               'M - Music|ML1 - ML3930 Literature on music',
+               'M - Music|ML1 - ML3930 Literature on music|ML93 - ML96.5 Manuscripts, autographs, etc.'
+             ])
+        )
+      end
     end
-  end
+
+    context 'no item having call number, but bib has LC call number in 050 or 090' do
+      it '(UNC) sets call_number_schemes to LC' do
+        rec = make_rec
+        rec << MARC::DataField.new('050', ' ', ' ',
+                                   ['a', 'ML96.4'],
+                                   ['b', '.B3'])
+
+        result = run_traject_on_record('unc', rec)['call_number_schemes']
+        expect(result).to(
+          eq(['LC'])
+        )
+      end
+      it '(UNC) uses first occurring LC call number to set shelfkey (and reverse) values' do
+        rec = make_rec
+        rec << MARC::DataField.new('050', ' ', ' ',
+                                   ['a', 'ML96.4'],
+                                   ['b', '.B3'])
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['v', 'Bd.1'])
+        result = run_traject_on_record('unc', rec)
+        expect(result['shelfkey']).to(
+          eq('lc:ML.00964.B3')
+        )
+        expect(result['reverse_shelfkey']).to(
+          eq('lc:DE}ZZQTV}OW')
+        )
+      end
+
+      it '(UNC) sets lcc_callnum_classification from both LC numbers' do
+        rec = make_rec
+        rec << MARC::DataField.new('090', ' ', ' ',
+                                   ['a', 'HV9468.G75'],
+                                   ['b', 'R33 2017'],
+                                   ['a', 'ML96.4'])
+
+        result = run_traject_on_record('unc', rec)['lcc_callnum_classification'].sort
+        expect(result).to(
+          eq([
+               'H - Social sciences',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration|HV8301 - HV9920.7 Penology. Prisons. Corrections',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration|HV8301 - HV9920.7 Penology. Prisons. Corrections|HV9441 - HV9920.7 By region or country',
+               'M - Music',
+               'M - Music|ML1 - ML3930 Literature on music',
+               'M - Music|ML1 - ML3930 Literature on music|ML93 - ML96.5 Manuscripts, autographs, etc.'
+             ])
+        )
+      end
+    end
+
+    context 'item has call number and bib has call number' do
+    it '(UNC) sets call_number_schemes to LC' do
+        rec = make_rec
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aML96.4|b.B3'],
+                                   ['v', 'Bd.1'])
+
+        rec << MARC::DataField.new('090', ' ', ' ',
+                                   ['a', 'HV9468.G75'],
+                                   ['b', 'R33 2017'])
+        result = run_traject_on_record('unc', rec)['call_number_schemes']
+        expect(result).to(
+          eq(['LC'])
+        )
+    end
+
+    it '(UNC) uses first occurring LC call number to set shelfkey (and reverse) values' do
+        rec = make_rec
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aML96.4|b.B3'],
+                                   ['v', 'Bd.1'])
+
+        rec << MARC::DataField.new('090', ' ', ' ',
+                                   ['a', 'HV9468.G75'],
+                                   ['b', 'R33 2017'])
+
+        result = run_traject_on_record('unc', rec)
+        expect(result['shelfkey']).to(
+          eq('lc:ML.00964.B3')
+        )
+        expect(result['reverse_shelfkey']).to(
+          eq('lc:DE}ZZQTV}OW')
+        )
+     end
+
+     it '(UNC) sets lcc_callnum_classification from both LC numbers' do
+        rec = make_rec
+        rec << MARC::DataField.new('999', '9', '1',
+                                   ['i', 'i1688265'],
+                                   ['p', '0501#'],
+                                   ['q', '|aML96.4|b.B3'],
+                                   ['v', 'Bd.1'])
+
+        rec << MARC::DataField.new('090', ' ', ' ',
+                                   ['a', 'HV9468.G75'],
+                                   ['b', 'R33 2017'])
+
+        result = run_traject_on_record('unc', rec)['lcc_callnum_classification'].sort
+        expect(result).to(
+          eq([
+               'H - Social sciences',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration|HV8301 - HV9920.7 Penology. Prisons. Corrections',
+               'H - Social sciences|HV1 - HV9960 Social pathology. Social and public welfare. Criminology|HV7231 - HV9960 Criminal justice administration|HV8301 - HV9920.7 Penology. Prisons. Corrections|HV9441 - HV9920.7 By region or country',
+               'M - Music',
+               'M - Music|ML1 - ML3930 Literature on music',
+               'M - Music|ML1 - ML3930 Literature on music|ML93 - ML96.5 Manuscripts, autographs, etc.'
+             ])
+        )
+    end
+    end
+
+
 end

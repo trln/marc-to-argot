f8091763a2f4e81d7d5b31f7bad742ae9d5983e9
Jacob Shelby <jtshelby@ncsu.edu>
2020-02-12 17:24:22 -0500

    corrected 581 mapping code

--- lib/marc_to_argot/macros/shared/notes.rb
+++ lib/marc_to_argot/macros/shared/notes.rb
@@ -383,11 +383,13 @@ module MarcToArgot
           labels << collect_subfield_values_by_code(field, '3').join(' ')
           labels << 'Related materials' if %w[0 1].include?(field.indicator1)
         when '581'
-          unless collect_subfield_values_by_code(field, '3').nil?
+          if field.subfields.map(&:code).include?('3')
+          # unless collect_subfield_values_by_code(field, '3').nil?
             labels << collect_subfield_values_by_code(field, '3').join(' ')
             labels << 'Publications relating to'
-          if collect_subfield_values_by_code(field, '3').nil?
+          else
             labels << 'Related publications'
+          end
         end
 
         labels.compact.reject(&:empty?).join(': ')

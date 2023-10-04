49b6f974d411ae31703c94c270bffde46db9d638
Derrek Croney <dlc32@duke.edu>
2023-10-04 16:51:08 -0400

    Add unproxied restricted support for Duke

--- spec/macros/duke/urls_spec.rb
+++ spec/macros/duke/urls_spec.rb
@@ -4,17 +4,12 @@ describe MarcToArgot::Macros::Duke::Urls do
   include described_class
 
   let(:restricted) { run_traject_json('duke', 'restricted', 'xml') }
-  let(:restricted3) { run_traject_json('duke', 'restricted3', 'xml') }
   let(:open_access) { run_traject_json('duke', 'open_access', 'xml') }
   let(:dc_urls) { run_traject_json('duke', 'dc_urls', 'xml') }
   let(:link_in_subfield_a) { run_traject_json('duke', 'link_in_subfield_a', 'xml') }
 
   context 'Duke' do
     it 'restricted is not set to false for fulltest URLs with the proxy prefix' do
-      expect(JSON.parse(restricted3['url'][0])['restricted']).to be_nil
-    end
-
-    it 'restricted is not set to false for whitelisted unproxied URLs' do
       expect(JSON.parse(restricted['url'][0])['restricted']).to be_nil
     end
 

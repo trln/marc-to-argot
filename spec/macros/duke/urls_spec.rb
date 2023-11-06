0e00dcf87013577b682c52f9487458f7e72eb528
Derrek Croney <dlc32@duke.edu>
2023-11-06 10:35:19 -0500

    Adds test spec for 'unproxied_restricted' resource

--- spec/macros/duke/urls_spec.rb
+++ spec/macros/duke/urls_spec.rb
@@ -4,13 +4,18 @@ describe MarcToArgot::Macros::Duke::Urls do
   include described_class
 
   let(:restricted) { run_traject_json('duke', 'restricted', 'xml') }
+  let(:unproxied_restricted) { run_traject_json('duke', 'unproxied_restricted', 'xml') }
   let(:open_access) { run_traject_json('duke', 'open_access', 'xml') }
   let(:dc_urls) { run_traject_json('duke', 'dc_urls', 'xml') }
   let(:link_in_subfield_a) { run_traject_json('duke', 'link_in_subfield_a', 'xml') }
 
   context 'Duke' do
-    it 'restricted is not set to false for fulltest URLs with the proxy prefix' do
+    it 'restricted is not set to false for fulltext URLs with the proxy prefix' do
       expect(JSON.parse(restricted['url'][0])['restricted']).to be_nil
+    end
+
+    it 'does not set restricted to false when an unproxied "Restricted" URL is detected' do
+      expect(JSON.parse(unproxied_restricted['url'][0])['restricted']).to be_nil
     end
 
     it 'sets restricted to false for open access URLs' do

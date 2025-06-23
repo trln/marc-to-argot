5d21304eabde372532926cd45ee8bd735dbdc237
Derrek Croney <dlc32@duke.edu>
2025-06-23 14:04:29 -0400

    refactored failed test spec

--- spec/macros/duke_macros_spec.rb
+++ spec/macros/duke_macros_spec.rb
@@ -9,28 +9,26 @@ describe MarcToArgot::Macros::Duke do
   let(:url_recs) { MARC::Reader.new(find_marc('duke', 'url', 'mrc')).to_a }
   let(:url_943_online) { run_traject_json('duke', 'url_943_journal_case', 'xml') }
   let(:expected_note) do
-    [['Duke law journal, v. 50, no. 6',
-      'Duke law journal, v. 50, no. 6',
-      'Duke law journal, v. 50, no. 6'],
-     [nil, nil],
-     ['Text version:', 'PDF version:'],
-     [nil, nil]]
+    [['Duke law journal, v. 50, no. 6'],
+     [nil],
+     ['Text version:'],
+     [nil]]
   end
   let(:expected_text) do
-    [[nil, nil, nil],
-     ['Collection guide', 'Finding aid'],
-     [nil, nil],
-     [nil, nil]]
+    [[nil],
+     ['Collection guide'],
+     [nil],
+     [nil]]
   end
 
   let(:expected_type) do
-    [%w[other other other],
-     %w[findingaid findingaid],
-     %w[fulltext fulltext],
-     %w[fulltext other]]
+    [%w[other],
+     %w[findingaid],
+     %w[fulltext],
+     %w[fulltext]]
   end
 
-  it 'extracts link note', skip: '856 fields (ALEPH) no longer expected' do
+  it 'extracts link note' do
     indexer.instance_eval do
       to_field 'url', url
     end
@@ -50,7 +48,7 @@ describe MarcToArgot::Macros::Duke do
     expect(url_943_online['access_type']).to include("Online")
   end
 
-  it 'extracts link text', skip: '856 fields (ALEPH) no longer expected' do
+  it 'extracts link text' do
     indexer.instance_eval do
       to_field 'url', url
     end
@@ -65,7 +63,7 @@ describe MarcToArgot::Macros::Duke do
     end
   end
 
-  it 'extracts link type', skip: '856 fields (ALEPH) no longer expected' do
+  it 'extracts link type' do
     indexer.instance_eval do
       to_field 'url', url
     end
